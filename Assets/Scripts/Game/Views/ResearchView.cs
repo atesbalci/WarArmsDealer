8fcc8f5bdc21867b731767118388c5959adbc0a2
Enlades <muratttkaan@gmail.com>
2018-02-05 17:26:51 +0300

    - Started adding ResearchView.

---
+++ Assets/Scripts/Game/Views/ResearchView.cs
@@ -0,0 +1,27 @@
+﻿using Game.Models;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+namespace Game.Views {
+    public class ResearchView : ViewBase {
+        public GameObject NewResearchPanel;
+
+        public List<ResearchActivity> CurrentResearchActivities;
+
+        private Company _company;
+
+        public void Bind(Company p_Company) {
+            CurrentResearchActivities = new List<ResearchActivity>();
+
+            _company = p_Company;
+
+            SetUiText();
+        }
+
+        private void SetUiText() {
+
+        }
+    }
+}
